b0b13a7e93082076eb13b3dc448aaa550001421d
LeitgebMathias <mathias.leitgeb@stud.fh-campuswien.ac.at>
2023-06-14 17:31:10 +0200

    Documentation added

--- src/main/java/at/ac/fhcampuswien/fhmdb/controllers/FactoryMainController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/controllers/FactoryMainController.java
@@ -5,14 +5,20 @@ import javafx.util.Callback;
 public class FactoryMainController implements Callback<Class<?>, Object> {
     private MainController controllerInstance;
 
+    // Der Konstruktor ist öffentlich, da es ja nicht nur eine Factory geben soll, sondern einen Controller
     public FactoryMainController() {
     }
 
+    // Call Methode wird immer vom JavaFX Framework aufgerufen, wenn ein Controller benötigt wird.
+    // In der Call-Methode ist dann das Singleton Pattern implementiert.
     @Override
     public Object call(Class<?> aClass) {
+        // Es wird nur dann eine neue Instanz erstellt, wenn diese noch nicht existiert.
         if (controllerInstance == null) {
             try {
                 System.out.println("Es wurde ein neuer MainController erstellt!");
+                // Da der Rückgabewert von newInstance in einen Maincontroller "getypecasted" wird,
+                // gibt es für jeden Controller eine eigene Factory.
                 controllerInstance = (MainController) aClass.getDeclaredConstructor().newInstance();
             } catch (Exception e) {
                 e.printStackTrace();
--- src/main/java/at/ac/fhcampuswien/fhmdb/controllers/FactoryMovieListController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/controllers/FactoryMovieListController.java
@@ -5,14 +5,20 @@ import javafx.util.Callback;
 public class FactoryMovieListController implements Callback<Class<?>, Object> {
     private MovieListController controllerInstance;
 
+    // Der Konstruktor ist öffentlich, da es ja nicht nur eine Factory geben soll, sondern einen Controller
     public FactoryMovieListController() {
     }
 
+    // Call Methode wird immer vom JavaFX Framework aufgerufen, wenn ein Controller benötigt wird.
+    // In der Call-Methode ist dann das Singleton Pattern implementiert.
     @Override
     public Object call(Class<?> aClass) {
+        // Es wird nur dann eine neue Instanz erstellt, wenn diese noch nicht existiert.
         if (controllerInstance == null) {
             try {
                 System.out.println("Es wurde ein neuer MovieListController erstellt!");
+                // Da der Rückgabewert von newInstance in einen MovieListController "getypecasted" wird,
+                // gibt es für jeden Controller eine eigene Factory.
                 controllerInstance = (MovieListController) aClass.getDeclaredConstructor().newInstance();
             } catch (Exception e) {
                 e.printStackTrace();
